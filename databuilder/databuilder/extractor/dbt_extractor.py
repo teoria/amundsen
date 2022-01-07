9b7de9b91de970a1fc3177404cb8f4fe61b11b92
Rodrigo Carneiro <teoria@gmail.com>
2022-01-07 17:02:46 -0300

    Revert "Update dbt_extractor.py"

--- databuilder/databuilder/extractor/dbt_extractor.py
+++ databuilder/databuilder/extractor/dbt_extractor.py
@@ -284,10 +284,8 @@ class DbtExtractor(Extractor):
 
         if self._extract_lineage:
             for upstream, downstreams in self._dbt_manifest['child_map'].items():
-                if upstream not in dbt_id_to_table_key:
-                    continue
                 valid_downstreams = [
-                    dbt_id_to_table_key[k] for k in downstreams if k.startswith(DBT_MODEL_PREFIX) and dbt_id_to_table_key.get(k)
+                    dbt_id_to_table_key[k] for k in downstreams if k.startswith(DBT_MODEL_PREFIX)
                 ]
                 if valid_downstreams:
                     yield TableLineage(
@@ -307,8 +305,8 @@ class DbtExtractor(Extractor):
         :returns: A list of `ColumnMetadata` in Amundsen.
         """
         tbl_columns = []
-        for catalog_col_name, catalog_col_content in catalog_columns.items():
-            manifest_col_content = manifest_columns.get(catalog_col_name, {})
+        for manifest_col_name, manifest_col_content in manifest_columns.items():
+            catalog_col_content = catalog_columns.get(manifest_col_name.upper())
 
             if catalog_col_content:
                 col_desc = None
